6496ca880b3ad945f93d57caec67d33606788b38
JamesIan-Bayonas <jamesianbayonas21@gmail.com>
2026-04-03 21:49:28 +0800

    feat: Add route for storing listings in the application

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\ListingController;
 use Illuminate\Support\Facades\Route;
 use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Auth;
@@ -52,6 +53,7 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+    Route::post('/listings', [ListingController::class, 'store'])->name('listings.store');
 });
 
 require __DIR__.'/auth.php';
